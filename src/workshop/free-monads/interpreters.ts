f6ae1d21a28c3cac40f710541a2ea37917861062
Yuriy Bogomolov <yuriy.bogomolov@gmail.com>
2020-04-25 19:16:47 +0300

    Implement Free Monads interpreters

--- src/workshop/free-monads/interpreters.ts
+++ src/workshop/free-monads/interpreters.ts
@@ -1,3 +1,58 @@
 /*
 Here you'll write concrete interpreters for your eDSL, which will translate it to the specific monad
 */
+
+import * as O from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/pipeable';
+import * as T from 'fp-ts/lib/Task';
+
+import { DBPost, john } from '../../domain';
+
+import { ProgramF } from './api';
+
+// type Interpreter = ProgramF ~> Task
+export const getTaskInterpreter = () => {
+  const cache = new Map<string, string>();
+  const dbFake: Record<number, Record<number, DBPost>> = {
+    1: {
+      1: { id: 1, title: 'First post', body: 'Look at this <b>cooool</b> post', tags: ['cool'], author: john },
+      2: { id: 2, title: 'Second post', body: 'Look at this even <b>cooooler</b> post', tags: ['cool'], author: john },
+    },
+  };
+
+  return <A>(program: ProgramF<A>): T.Task<A> => {
+    switch (program.tag) {
+      case 'KVGet': return T.task.fromIO(() => program.next(O.fromNullable(cache.get(program.key))));
+      case 'KVPut': return T.task.fromIO(() => program.next(cache.set(program.key, program.value) != null));
+      case 'KVDelete': return T.task.fromIO(() => program.next(cache.delete(program.key)));
+
+      case 'DBGetPosts': return T.delay(400)(T.task.fromIO(() => program.next(Object.values(dbFake[program.userId]))));
+      case 'DBCreatePost': {
+        const userPostDb = dbFake[program.post.author.id];
+        const id = Math.max(...Object.keys(userPostDb).map(k => +k)) + 1;
+        const newPost = { ...program.post, id };
+        dbFake[program.post.author.id][id] = newPost;
+        return T.delay(150)(T.task.fromIO(() => program.next(newPost)));
+      }
+      case 'DBUpdatePost': return T.delay(200)(pipe(
+        O.fromNullable(dbFake[program.update.author.id][program.postId]),
+        O.fold(
+          () => T.task.of(program.next(O.none)),
+          (existingPost) => {
+            const updatedPost = { ...existingPost, ...program.update };
+            dbFake[program.update.author.id][existingPost.id] = updatedPost;
+            return T.task.fromIO(() => program.next(O.some(updatedPost)));
+          },
+        ),
+      ));
+
+      case 'NetSend': return T.delay(1500)(T.task.fromIO(() => program.next()));
+    }
+  };
+};
+
+export const logger = <A>(program: ProgramF<A>): ProgramF<A> => {
+  const { tag, ...rest } = program;
+  console.log(`[${tag}]`, JSON.stringify(rest));
+  return program;
+};
--- src/workshop/free-monads/run.ts
+++ src/workshop/free-monads/run.ts
@@ -1,12 +1,20 @@
-import { notImplemented } from '../../utils/throw';
+import * as F from 'fp-ts-contrib/lib/Free';
+import { flow } from 'fp-ts/lib/function';
+import * as T from 'fp-ts/lib/Task';
+
+import { coolPost, john } from '../../domain';
+
+import { exampleProgram1, exampleProgram2, exampleProgram3 } from './examples';
+import { getTaskInterpreter, logger } from './interpreters';
 
 (async () => {
-  console.log('exampleProgram1: first run');
-  await notImplemented();
-  console.log('exampleProgram1: second run');
-  await notImplemented();
+  const taskI = getTaskInterpreter();
+  const interpreter = flow(logger, taskI);
+
+  console.log('exampleProgram1');
+  await F.foldFree(T.task)(interpreter, exampleProgram1(john.id))();
   console.log('exampleProgram2');
-  await notImplemented();
+  await F.foldFree(T.task)(interpreter, exampleProgram2(coolPost))();
   console.log('exampleProgram3');
-  await notImplemented();
+  await F.foldFree(T.task)(interpreter, exampleProgram3(1, coolPost))();
 })();
